9f228a56f4b09f7ea1fb710c67a3194516790b7b
MaXoooZ <55662431+Max-xoo@users.noreply.github.com>
2020-12-31 22:29:21 +0100

    Update javascript.js

--- fizz-buzz/javascript.js
+++ fizz-buzz/javascript.js
@@ -4,9 +4,9 @@ for (let i = 0; i < 101; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(i + ` ${fizz}+${buzz}`);
-    } else if(i % 3 === 0) {
+    } else if (i % 3 === 0) {
         console.log(i + ` ${fizz}`);
-    } else if(i % 5 === 0) {
+    } else if (i % 5 === 0) {
         console.log(i + ` ${buzz}`);
     } else {
         console.log(i);
