4fe4299147787a7658c7824a54302bd12a51b4ea
AlexOZx <69150061+Galzron@users.noreply.github.com>
2020-12-31 23:42:04 +0100

    Create python2.py

---
+++ fizz-buzz/python2.py
@@ -0,0 +1,9 @@
+for i in xrange(1, 100):
+    if i % 3 == 0  && i % 5 == 0:
+        print "FizzBuzz"
+    elif i % == 0:
+        print "Fizz"
+    elif i % 5 == 0
+        print "Buzz"
+    else: 
+        print i
