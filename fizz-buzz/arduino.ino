2771b1225f30e819b846f50b9d6904c8dfdca70c
AlexOZx <69150061+Galzron@users.noreply.github.com>
2021-01-01 00:24:21 +0100

    change order

--- fizz-buzz/arduino.ino
+++ fizz-buzz/arduino.ino
@@ -6,9 +6,9 @@ void loop() {
     for(int i = 1; i <= 100; i++) {
         if (i % 5 == 0 && i % 3 == 0) {
             Serial.println("FizzBuzz")
-        } else if (i % 5 == 0) {
-            Serial.println("Fizz")
         } else if (i % 3 == 0) {
+            Serial.println("Fizz")
+        } else if (i % 5 == 0) {
             Serial.println("Buzz")
         } else {
             Serial.println(i)
