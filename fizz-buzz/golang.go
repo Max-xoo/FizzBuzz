9a40687ebd88af4eda48e8457d1a1c92fc008757
MaXoooZ <55662431+Max-xoo@users.noreply.github.com>
2020-12-31 21:24:01 +0100

    Create golang.go

---
+++ fizz-buzz/golang.go
@@ -0,0 +1,25 @@
+package golang
+
+import (
+	"flag"
+	"fmt"
+)
+
+
+func main() {
+	flag.Parse()
+	for i := 1; i <= 100; i++ {
+		fizz := "fizz"
+		buzz := "buzz"
+
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, fizz+buzz)
+		} else if i%3 == 0 {
+			fmt.Println(i, fizz)
+		} else if i%5 == 0 {
+			fmt.Println(i, buzz)
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
