3c7a0d7816a6e8f5bf8318776ba9c479184f18a3
MaXoooZ <55662431+Max-xoo@users.noreply.github.com>
2020-12-31 23:33:38 +0100

    Update julia.jl

--- fizz-buzz/julia.jl
+++ fizz-buzz/julia.jl
@@ -1,8 +1,8 @@
 for i in 1:100
     if i % 5 == 0 && i % 3 == 0
-        println(i, "fizz+buzz")
+        println(i, " fizz+buzz")
     elseif i % 3 == 0
-        println(i, "fizz")
+        println(i, " fizz")
     elseif i % 5 == 0
         println(i, " buzz")
     else 
