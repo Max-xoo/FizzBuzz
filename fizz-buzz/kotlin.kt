c91bd2f7c7069fabdd4baf07b96fa66efd3a6202
Alex_ <69150061+KTsuyuri@users.noreply.github.com>
2021-01-16 19:22:50 +0100

    Update kotlin.kt

--- fizz-buzz/kotlin.kt
+++ fizz-buzz/kotlin.kt
@@ -2,9 +2,9 @@ fun fizzBuzz() {
     for (i in 1..100) {
         println(
             when {
-                i % 3 == 0 && i % 5 == 0 -> i + " fizz+buzz"
-                i % 3 == 0 -> i + " fizz"
-                i % 5 == 0 -> i + " buzz"
+                i % 3 == 0 && i % 5 == 0 -> "fizzbuzz"
+                i % 3 == 0 -> "fizz"
+                i % 5 == 0 -> "buzz"
                 else -> i
             }
         )
