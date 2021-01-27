a412147b10c1d544996466dd1e1c8dc2eab735ff
Alex_ <69150061+Kepura@users.noreply.github.com>
2021-01-27 21:04:50 +0100

    Update golang.go

--- fizz-buzz/golang.go
+++ fizz-buzz/golang.go
@@ -1,22 +1,17 @@
-package golang
+package main
 
 import (
-	"flag"
 	"fmt"
 )
 
 func main() {
-	flag.Parse()
 	for i := 1; i <= 100; i++ {
-		fizz := "fizz"
-		buzz := "buzz"
-
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println(i, fizz+buzz)
-		} else if i%3 == 0 {
-			fmt.Println(i, fizz)
-		} else if i%5 == 0 {
-			fmt.Println(i, buzz)
+		if i % 3 == 0 && i % 5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
 		} else {
 			fmt.Println(i)
 		}
